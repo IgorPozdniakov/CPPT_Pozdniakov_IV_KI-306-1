a10637624878293cf86aef8a076d1f12900f55c3
Igor_Pozdniakov <igorpozdniakov726@gmail.com>
2023-12-02 13:17:47 +0200

    Lab2 commit

---
+++ Lab2/src/lab2/Dynamics.java
@@ -0,0 +1,14 @@
+package lab2;
+
+public class Dynamics {
+    private byte loudness = 50;
+
+    public byte getLoudness() {
+        return loudness;
+    }
+
+    public void setLoudness(byte loudness) {
+        this.loudness = loudness;
+    }
+}
+
